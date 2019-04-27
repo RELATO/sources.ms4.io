73171ef4626258f8da8e9413a1dd8116d6d9655e
root <root@server3.oncloud.digital>
2019-04-27 14:38:15 +0000

    More information added

--- 3datacenters/providers.tf
+++ 3datacenters/providers.tf
@@ -232,3 +232,84 @@ output "vtr-hostnames" {
 output "vtr-public_ips" {
   value = ["${vultr_instance.intance.*.ipv4_address}"]
 }
+
+resource rke_cluster "cluster" {
+  nodes {
+      address = "${scaleway_server.scw-host.0.public_ip}"
+      user    = "rke"
+#      internal_address = "${scaleway_server.scw-host.0.private_ip}"
+      role    = ["etcd"]
+  }
+  nodes {
+      address = "${digitalocean_droplet.dgo-host.0.ipv4_address}"
+      user    = "rke"
+#      internal_address = "${digitalocean_droplet.dgo-host.0.ipv4_address}"
+      role    = ["etcd"]
+  }
+  nodes {
+      address = "${vultr_instance.intance.0.ipv4_address}"
+      user    = "rke"
+#      internal_address = "${vultr_instance.intance.0.ipv4_address}"
+      role    = ["etcd"]
+  }
+  nodes {
+      address = "${scaleway_server.scw-host.1.public_ip}"
+      user    = "rke"
+#      internal_address = "${scaleway_server.scw-host.1.private_ip}"
+      role    = ["worker","controlplane"]
+  }
+  nodes {
+      address = "${digitalocean_droplet.dgo-host.1.ipv4_address}"
+      user    = "rke"
+#      internal_address = "${digitalocean_droplet.dgo-host.1.ipv4_address}"
+      role    = ["worker","controlplane"]
+  }
+  nodes {
+      address = "${vultr_instance.intance.1.ipv4_address}"
+      user    = "rke"
+#      internal_address = "${vultr_instance.intance.1.ipv4_address}"
+      role    = ["worker","controlplane"]
+  }
+  nodes {
+      address = "${scaleway_server.scw-host.2.public_ip}"
+      user    = "rke"
+#      internal_address = "${scaleway_server.scw-host.2.private_ip}"
+      role    = ["worker"]
+  }
+  nodes {
+      address = "${digitalocean_droplet.dgo-host.2.ipv4_address}"
+      user    = "rke"
+#      internal_address = "${digitalocean_droplet.dgo-host.2.ipv4_address}"
+      role    = ["worker"]
+  }
+  nodes {
+      address = "${vultr_instance.intance.2.ipv4_address}"
+      user    = "rke"
+#      internal_address = "${vultr_instance.intance.2.ipv4_address}"
+      role    = ["worker"]
+  }
+}
+
+###############################################################################
+# If you need kubeconfig.yml for using kubectl, please uncomment follows.
+###############################################################################
+resource "local_file" "kube_cluster_yaml" {
+  filename = "${path.root}/kube_config_cluster.yml"
+  content  = "${rke_cluster.cluster.kube_config_yaml} "
+}
+
+###############################################################################
+# If you need cluster.yml for using rke, please uncomment follows.
+###############################################################################
+resource "local_file" "rke_cluster_yaml" {
+  filename = "${format("%s/%s", path.root, "cluster.yml")}"
+  content = "${rke_cluster.cluster.rke_cluster_yaml}"
+}
+
+###############################################################################
+# You can also use an output.
+###############################################################################
+output "rke_cluster_yaml" {
+  sensitive = true
+  value = "${rke_cluster.cluster.rke_cluster_yaml}"
+}
